21bfd95dc37a89219ae71e3d8d9b7e971e832722
chandru-engineer <tochandru.engineer@gmail.com>
2024-02-23 10:47:27 +0530

    day-2

--- styling_app/App.js
+++ styling_app/App.js
@@ -4,9 +4,14 @@ import { View, Text, StyleSheet } from 'react-native'
 export default function App() {
     return (
         <View style={styles.container}>
-
-            <View style={styles.BlueboxContainer}>
-                <Text>Style App</Text>
+            <View style={[styles.darkMode]}>
+                <Text style={[styles.darkModetext]}>Style App</Text>
+            </View>
+            <View style={[styles.BlueboxContainer, styles.box, styles.boxShadow, styles.androidShadow]}>
+                <Text style={{borderRadius: 5, backgroundColor: 'red'}}>Style App</Text>
+            </View>
+            <View style={[styles.YellowboxContainer, styles.box]}>
+                <Text  style={{borderRadius: 5, backgroundColor: 'yellow'}}>Style App</Text>
             </View>
         </View>
     )
@@ -16,11 +21,43 @@ export default function App() {
 const styles = StyleSheet.create(
     {
         container: { flex: 1, backgroundColor: "plum", padding: 60 },
-        BlueboxContainer: {
-            backgroundColor: 'yellow',
-            height: 100,
-            width: 100,
+        darkMode: {
+            backgroundColor: 'black',
+            
+        },
+        darkModetext: {
             color: "white"
+        },
+        BlueboxContainer: {
+            backgroundColor: 'lightblue',
+
+        },
+        YellowboxContainer: {
+            backgroundColor: 'white',
+        },
+        box: {
+            height: 250,
+            width: 250,
+            padding: 0,
+            paddingHorizontal: 10,
+            paddingVertical: 20,
+            marginHorizontal: 10,
+            marginVertical: 10,
+            borderWidth: 2,
+            borderColor: 'purple',
+            borderRadius: 5, 
+        },
+        boxShadow: {
+            shadowColor: "#333333",
+            shadowOffset:{
+                width: 6,
+                height: 6
+            },
+            shadowOpacity: 1,
+            shadowRadius: 4,
+        },
+        androidShadow: {
+            elevation: 10,
         }
     }
 )
